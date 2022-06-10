bf612269216f0b100d73aaab60eeba1703aee391
taiki <>
2022-06-10 11:33:32 +0900

    backend cors frontend Api用defaultURL修正

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /my-household-8a77e\.web\.app/
+    origins /localhost:3001/
     resource '*',
       headers: :any,
       expose: ["access-token", "expiry", "token-type", "uid", "client"],
